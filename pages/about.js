a096395d98bb63f642ad39d680399adb7536a58c
= <stefan19052003@gmail.com>
2023-06-09 23:04:15 +0200

    fix again

--- pages/about.js
+++ pages/about.js
@@ -24,7 +24,7 @@ const About = () => {
             {
               mySkills.map((skill) =>{
                 return(
-                  <div className="bg-black w-32 h-64">
+                  <div className="bg-black w-32 h-64" key={skill}>
                     jasdklfja;ldfkj
                   </div>
                 )
